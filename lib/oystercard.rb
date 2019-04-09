c67ec4329e3ae55a16e2ff2c2c2d43bca5a0f72c
Ayelisha Amoah <ayelisha.amoah@gmail.com>
2019-04-09 16:49:25 +0100

    Updated deduct to be a private method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -15,10 +15,6 @@ class Oystercard
     @balance += amount
   end
 
-  def deduct(amount)
-    @balance -= amount
-  end
-
   def in_journey?
     @in_journey
   end
@@ -33,4 +29,8 @@ class Oystercard
     @in_journey = false
   end
 
+  private
+    def deduct(amount)
+      @balance -= amount
+    end
 end
