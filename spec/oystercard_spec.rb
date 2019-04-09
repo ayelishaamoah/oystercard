4f093fa56a0b91b40df1bcd145b663c79b51d0a8
Ayelisha Amoah <ayelisha.amoah@gmail.com>
2019-04-09 15:33:33 +0100

    Refactored tests to use .to change matcher for top up and deduct tests

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,18 +5,18 @@ describe Oystercard do
 
   describe '#top_up' do
     it 'balance can be topped up' do
-      expect(subject.top_up(10)).to eq 10
+      expect{ subject.top_up(25) }.to change{ subject.balance }.by +25
     end
 
     it 'fails if user tries to exceed maximum balance' do
-      expect{subject.top_up(95)}.to raise_error("Maximum balance of #{Oystercard::MAX_BALANCE} exceeded")
+      expect{ subject.top_up(95) }.to raise_error("Maximum balance of #{Oystercard::MAX_BALANCE} exceeded")
     end
   end
 
   describe '#deduct' do
     it 'allows the user to deduct money from the balance' do
       subject.top_up(50)
-      expect(subject.deduct(25)).to eq 25
+      expect{ subject.deduct(25) }.to change { subject.balance }.by -25
     end
   end
 
