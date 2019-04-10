6e24196722481e90d4fa3bf6abe038161c55b207
Ayelisha Amoah <ayelisha.amoah@gmail.com>
2019-04-10 14:37:19 +0100

    Added entry station instance varible

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,5 +1,5 @@
 class Oystercard
-  attr_reader :balance
+  attr_reader :balance, :entry_station
 
   MAX_BALANCE = 90
   MIN_BALANCE = 1
@@ -19,9 +19,10 @@ class Oystercard
     @in_journey
   end
 
-  def touch_in
+  def touch_in(station)
     raise "Your current balance is less than the minumum fare" if @balance < MIN_BALANCE
     @in_journey = true
+    @entry_station = station
   end
 
   def touch_out
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,6 +2,8 @@ require 'oystercard'
 
 describe Oystercard do
 
+let(:station) { double :station }
+
   describe '#top_up' do
     it 'balance can be topped up' do
       expect{ subject.top_up(25) }.to change{ subject.balance }.by +25
@@ -21,19 +23,24 @@ describe Oystercard do
   describe '#touch_in' do
     it 'should update the card status to be in journey' do
       subject.top_up(50)
-      subject.touch_in
+      subject.touch_in(station)
       expect(subject).to be_in_journey
     end
 
+      it 'should record the entry station' do
+        subject.top_up(25)
+        expect(subject.touch_in(station)).to eq(station)
+      end
+
     it 'throws an error if balance is lower than minimun fare' do
-      expect{ subject.touch_in }.to raise_error "Your current balance is less than the minumum fare"
+      expect{ subject.touch_in(station) }.to raise_error "Your current balance is less than the minumum fare"
     end
   end
 
   describe '#touch_out' do
-    it 'should update the card status to be in journey' do
+    it 'should update the card status to not be in journey' do
       subject.top_up(50)
-      subject.touch_in
+      subject.touch_in(station)
       subject.touch_out
       expect(subject).not_to be_in_journey
     end
@@ -42,5 +49,4 @@ describe Oystercard do
       expect { subject.touch_out }.to change{ subject.balance }.by (- Oystercard::MIN_BALANCE)
     end
   end
-
 end
